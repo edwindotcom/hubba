4e2178bd875c8e94bbe31257ef0296ecb29ddf57
Edwin Wong <edwinwongsf@gmail.com>
2020-04-06 14:49:56 -0700

    moar files

---
+++ build/precache-manifest.03236b40cb9899eed19df5cad1410a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5901f9145eba5170645af01397acc8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95cbe20209b76b1c70f4",
+    "url": "/static/css/main.1d484105.chunk.css"
+  },
+  {
+    "revision": "69bcaeeb96e13f51dc15",
+    "url": "/static/js/2.dadbef7c.chunk.js"
+  },
+  {
+    "revision": "95cbe20209b76b1c70f4",
+    "url": "/static/js/main.b725d9c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f905b4113f43675084932ed3b43ee429",
+    "url": "/static/media/octoface.f905b411.svg"
+  }
+]);
